aecf8251d3abdc36b385bd792ee0d37e0ee46b49
Taras Honcharuk <taras.goncharuk22@gmail.com>
2021-02-18 00:18:19 +0200

    change logic

--- task 1/script.js
+++ task 1/script.js
@@ -1,32 +1,20 @@
-const container = document.getElementById('container');
 const input = document.getElementById('input');
 const buttonAdd = document.getElementById('buttonAdd');
 const buttonRemove = document.getElementById('buttonRemove');
 const text = document.getElementById('text');
 
 
-let finalText = ['apple', 'grapes', 'peach'];
-
 function addEventToInput(){
-    let value = input.value;
-    finalText.push(value.split());
-    text.innerText = finalText.join(' ');
+  input.addEventListener('input', startAdd)
 }
 
 function removeEventToInput(){
-    let value = input.value;
-
-    if (value !== '') {
-        input.value = '';
-        console.log(value);
-  }
-    if (value == '') {
-        finalText.pop();
-        text.innerText = finalText.join(' ');
-    }
+    input.removeEventListener('input', startAdd)
 }
 
-text.innerText = finalText.join(' ');
+function startAdd(e) {
+    text.innerText = e.target.value
+}
 
 buttonAdd.addEventListener('click', addEventToInput);
 buttonRemove.addEventListener('click', removeEventToInput);
