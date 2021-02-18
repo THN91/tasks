725b44ce31452d6898e7cfb46a9e17daa7da3a7b
Taras Honcharuk <taras.goncharuk22@gmail.com>
2021-02-19 00:50:12 +0200

    in progress

---
+++ task 3/script.js
@@ -0,0 +1,42 @@
+const item = document.getElementById('item');
+const btn = document.getElementById('btn');
+const addBlock = document.getElementById('addBlock');
+const list = document.getElementById('list');
+
+function clickItem () {
+    console.log('click item');
+}
+
+function clickBtn (event) {
+    event.stopPropagation();
+    console.log('click button')
+}
+
+function count() {
+
+}
+
+function newBlock () {
+    let wrapperItem = document.createElement('div');
+    wrapperItem.className = 'wrapper-item';
+
+    let test = document.createElement('div');
+    test.className = 'test';
+    test.innerText = `Test ${4}`;
+
+    let btn = document.createElement('div');
+    btn.className = 'btn';
+
+    let icon = document.createElement('div');
+    icon.className = 'fas fa-cog';
+
+
+    wrapperItem.appendChild(test);
+    wrapperItem.appendChild(btn);
+    btn.appendChild(icon);
+    list.appendChild(wrapperItem);
+}
+
+item.addEventListener('click', clickItem);
+btn.addEventListener('click', clickBtn);
+addBlock.addEventListener('click', newBlock);
