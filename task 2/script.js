e819c369c489f52edd59438cb523bd43ddb8c456
FreeNikita <freenikita95@gmail.com>
2021-02-18 14:27:47 +0200

    some fix

---
+++ task 2/script.js
@@ -0,0 +1,10 @@
+const click = document.getElementById('item2');
+const click1 = document.getElementById('item21');
+
+click.addEventListener('click', () => {
+    console.log('test')
+})
+
+click1.addEventListener('click', () => {
+    console.log('test1')
+})
